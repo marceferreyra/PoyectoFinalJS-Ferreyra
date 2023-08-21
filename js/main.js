56f004c58c8b5946c1d125f58e5dcd6f2732d43a
Marcelo Ferreyra <marceloferreyra@outlook.com>
2023-08-21 19:31:10 -0300

    agrego opcion de quitar productos, coloco link js debajo del cdn de boostrap

--- js/main.js
+++ js/main.js
@@ -1,190 +1,3 @@
-/*let ingreso;
-let contador = 0;
-let seleccionFlorales = false
-let seleccionAmaderadas = false
-let seleccionFrutales = false
-let seleccionCítricas = false
-let seleccionRefrescante = false
-let seleccionAhumadas = false
-let seleccionListo = false
-let seRealizoSeleccion = false;
-
-for (let contador = 1; contador <= 3;) {
-    ingreso = prompt(`Selecciona tus fragancias (puedes elegir hasta tres):\n1- Florales\n2- Amaderadas\n3- Frutales\n4- Cítricas\n5- Refrescantes\n6- Ahumadas\n0- Listo`);
-
-    switch (ingreso) {
-        case "1":
-            alert("Seleccionaste Florales");
-            seleccionFlorales = true;
-            contador++;
-            seRealizoSeleccion = true;
-            break;
-        case "2":
-            alert("Seleccionaste Amaderadas");
-            seleccionAmaderadas = true;
-            contador++;
-            seRealizoSeleccion = true;
-            break;
-        case "3":
-            alert("Seleccionaste Frutales");
-            seleccionFrutales = true;
-            contador++;
-            seRealizoSeleccion = true;
-            break;
-        case "4":
-            alert("Seleccionaste Cítricas");
-            seleccionCítricas = true;
-            contador++;
-            seRealizoSeleccion = true;
-            break;
-        case "5":
-            alert("Seleccionaste Refrescantes");
-            seleccionRefrescante = true;
-            contador++;
-            seRealizoSeleccion = true;
-            break;
-        case "6":
-            alert("Seleccionaste Ahumadas");
-            seleccionAhumadas = true;
-            contador++;
-            seRealizoSeleccion = true;
-            break;
-        case "0":
-            alert("Listo");
-            seleccionListo = true;
-            contador++;
-            break;
-        default:
-            alert("Opción no válida");
-            break;
-    }
-
-    if (ingreso === "0") {
-        break;
-    }
-}
-
-if (seleccionFlorales && seleccionCítricas && seleccionAhumadas) {
-    comprarRecomendacion("Phantom", 54650);
-} else if (seleccionFlorales && seleccionCítricas && seleccionAmaderadas) {
-    comprarRecomendacion("L´eau D´Issey Pour Homme", 58850);
-} else if (seleccionFrutales && seleccionCítricas && seleccionRefrescante) {
-    comprarRecomendacion("Polo Blue Ralph Lauren", 54315);
-} else if (seleccionFlorales && seleccionFrutales && seleccionAmaderadas) {
-    comprarRecomendacion("One Million", 52550);
-} else if (seleccionAmaderadas && seleccionFlorales && seleccionRefrescante) {
-    comprarRecomendacion("L´Homme Yves Saint Laurent", 59415);
-} else if (seleccionRefrescante && seleccionCítricas && seleccionAhumadas) {
-    comprarRecomendacion("Dolce & Gabbana Light Blue Pour Homme", 60750);
-} else if (seleccionFlorales && seleccionRefrescante && seleccionListo) {
-    comprarRecomendacion("Gentleman Gyvenchi", 66200);
-} else if (seleccionCítricas && seleccionRefrescante && seleccionAmaderadas) {
-    comprarRecomendacion("Lacoste L.12.12 Blanc", 35435);
-} else if (seleccionCítricas && seleccionAhumadas && seleccionAmaderadas) {
-    comprarRecomendacion("Eau de Parfum Boss Bottled Parfum", 63000);
-} else if (seleccionAmaderadas && seleccionCítricas && seleccionListo) {
-    comprarRecomendacion("Dior Eau de Parfum Sauvage", 67000);
-} else if (seRealizoSeleccion === false && ingreso === "0") {
-    alert("No has seleccionado ninguna fragancia. Refresca la página si quieres completar tus preferencias.");
-} else {
-    alert("Lo senitmos!! \nNo tenemos fragancias con tus preferencias en estos momentos");
-}
-
-function comprarRecomendacion(producto, precio) {
-    let opcion;
-    while (true) {
-        opcion = prompt(`Te recomendamos ${producto} a un precio de $${precio}. ¿Deseas comprarlo?\n1- Si\n2- No`);
-        switch (opcion) {
-            case "1":
-                opcion = true;
-                break;
-            case "2":
-                opcion = false;
-                alert("Gracias, tus preferencias han sido registradas. ");
-                return;
-            default:
-                alert("Opción no válida. Por favor, seleccione '1' para Si o '2' para No.");
-                continue;
-        }
-        break;
-    }
-
-    if (opcion) {
-        let cantidad;
-        while (true) {
-            cantidad = parseInt(prompt("Ingrese la cantidad que desea comprar:"));
-            if (isNaN(cantidad) === false && cantidad > 0) {
-                break;
-            }
-            alert("Por favor, ingrese una cantidad válida mayor que cero.");
-        }
-
-        let pagarEnCuotas;
-        switch (prompt("¿Deseas pagar en cuotas?\n1- Si\n2- No")) {
-            case "1":
-                pagarEnCuotas = true;
-                break;
-            case "2":
-                pagarEnCuotas = false;
-                break;
-            default:
-                alert("Opción no válida.");
-                pagarEnCuotas = false;
-                break;
-        }
-        let cuotasSeleccionadas;
-        if (pagarEnCuotas) {
-            switch (prompt("Seleccione la cantidad de cuotas. \n1- 3 cuotas sin interés \n2- 6 cuotas (10% de interés) \n3- 12 cuotas (20% de interés)")) {
-                case "1":
-                    cuotasSeleccionadas = 3;
-                    break;
-                case "2":
-                    cuotasSeleccionadas = 6;
-                    break;
-                case "3":
-                    cuotasSeleccionadas = 12;
-                    break;
-                default:
-                    alert("Opción no válida. Por favor, seleccione una cantidad de cuotas válida");
-            }
-        } else {
-            alert(`Vas a pagar $${precio * cantidad} en un solo pago`);
-        }
-
-        if (cuotasSeleccionadas === 3) {
-            let montoCuota = (precio * cantidad) / cuotasSeleccionadas;
-            alert(`Vas a pagar 3 cuotas de $${montoCuota.toFixed(2)}`);
-        } else if (cuotasSeleccionadas === 6) {
-            let montoCuota = (precio * cantidad * 1.10) / cuotasSeleccionadas;
-            alert(`Vas a pagar 6 cuotas de $${montoCuota.toFixed(2)}`);
-        } else if (cuotasSeleccionadas === 12) {
-            let montoCuota = (precio * cantidad * 1.20) / cuotasSeleccionadas;
-            alert(`Vas a pagar 12 cuotas de $${montoCuota.toFixed(2)}`);
-        }
-    }
-    let confirmarCompra;
-    while (true) {
-        switch (prompt("¿Deseas confirmar la compra?\n1- Si\n2- No")) {
-            case "1":
-                confirmarCompra = true;
-                break;
-            case "2":
-                confirmarCompra = false;
-                alert("Gracias, puedes finalizar tu compra en otro momento.");
-                break;
-            default:
-                alert("Opción no válida. Por favor, seleccione '1' para Si o '2' para No.");
-                continue;
-        }
-        break;
-    }
-
-    if (confirmarCompra) {
-        alert("¡Muchas gracias por su compra!");
-    }
-}
-*/
-
 const perfumes = [
     { nombre: "EDP Sauvage Dior", precio: 67000 },
     { nombre: "EDP Boss Bottled Boss", precio: 63000 },
@@ -290,12 +103,46 @@ while (true) {
     } else if (opcion === 3) {
         if (carrito.length > 0) {
             let detallesCarrito = "Productos en el carrito:\n";
-            for (const producto of carrito) {
-                detallesCarrito += `${producto.nombre} - $${producto.precio}\n`;
+            for (let i = 0; i < carrito.length; i++) {
+                detallesCarrito += `${i + 1}- ${carrito[i].nombre} - $${carrito[i].precio}\n`;
             }
             const total = carrito.reduce((acc, el) => acc + el.precio, 0);
-            detallesCarrito += `\nTotal: $${total}`;
-            alert(detallesCarrito);
+            detallesCarrito += `\nTotal: $${total}\n\nSelecciona una opción:\n1- Volver al menú principal\n2- Quitar producto del carrito`;
+    
+            while (true) {
+                const confirmacionCarrito = parseInt(prompt(detallesCarrito));
+    
+                switch (confirmacionCarrito) {
+                    case 1:
+                        break; 
+                    case 2:
+                        while (true) {
+                            let productosParaQuitar = "Selecciona el producto que deseas quitar:\n";
+                            for (let i = 0; i < carrito.length; i++) {
+                                productosParaQuitar += `${i + 1}- ${carrito[i].nombre} - $${carrito[i].precio}\n`;
+                            }
+                            productosParaQuitar += `${carrito.length + 1}- Volver al menú principal`;
+    
+                            const productoAQuitar = parseInt(prompt(productosParaQuitar)) - 1;
+                            if (productoAQuitar >= 0 && productoAQuitar < carrito.length) {
+                                const productoQuitado = carrito.splice(productoAQuitar, 1);
+                                alert(`Producto ${productoQuitado[0].nombre} ha sido quitado del carrito.`);
+                            } else if (productoAQuitar === carrito.length) {
+                                break;
+                            } else {
+                                alert("Opción no válida. Por favor, selecciona una opción válida.");
+                            }
+                        }
+                        break;
+                    default:
+                        alert("Opción no válida. Por favor, selecciona una opción válida.");
+                        break;
+                }
+    
+                if (confirmacionCarrito === 1 || confirmacionCarrito === 2) {
+                    break;
+                }
+            }
         } else {
             alert("El carrito está vacío.");
         }
@@ -303,28 +150,47 @@ while (true) {
         if (carrito.length > 0) {
             const total = carrito.reduce((acc, el) => acc + el.precio, 0);
             let detallesCarrito = "Productos en el carrito:\n";
-            for (const producto of carrito) {
-                detallesCarrito += `${producto.nombre} - $${producto.precio}\n`;
+            for (let i = 0; i < carrito.length; i++) {
+                detallesCarrito += `${i + 1}- ${carrito[i].nombre} - $${carrito[i].precio}\n`;
             }
-            detallesCarrito += `\nTotal: $${total}`;
-
+            detallesCarrito += `\nTotal: $${total}\n\nSelecciona una opción:\n1- Finalizar compra\n2- Volver al menú principal\n3- Quitar producto del carrito`;
+    
             while (true) {
-                const confirmacionFinalizar = prompt(`${detallesCarrito}\n\n¿Deseas finalizar la compra?\n\n1- Si\n2- No`);
-
+                const confirmacionFinalizar = parseInt(prompt(detallesCarrito));
+    
                 switch (confirmacionFinalizar) {
-                    case "1":
+                    case 1:
                         alert("¡Compra finalizada! Gracias por tu compra.");
                         carrito = [];
                         break;
-                    case "2":
+                    case 2:
                         alert("No has finalizado la compra. Todavía hay productos en tu carrito.");
                         break;
+                    case 3:
+                        while (true) {
+                            let productosParaQuitar = "Selecciona el producto que deseas quitar:\n";
+                            for (let i = 0; i < carrito.length; i++) {
+                                productosParaQuitar += `${i + 1}- ${carrito[i].nombre} - $${carrito[i].precio}\n`;
+                            }
+                            productosParaQuitar += `${carrito.length + 1}- Volver al menú principal`;
+            
+                            const productoAQuitar = parseInt(prompt(productosParaQuitar)) - 1;
+                            if (productoAQuitar >= 0 && productoAQuitar < carrito.length) {
+                                const productoQuitado = carrito.splice(productoAQuitar, 1);
+                                alert(`Producto ${productoQuitado[0].nombre} ha sido quitado del carrito.`);
+                            } else if (productoAQuitar === carrito.length) {
+                                break; 
+                            } else {
+                                alert("Opción no válida. Por favor, selecciona una opción válida.");
+                            }
+                        }
+                        break;
                     default:
-                        alert("Opción no válida. Por favor, selecciona 'Si' o 'No'.");
+                        alert("Opción no válida. Por favor, selecciona una opción válida.");
                         break;
                 }
-
-                if (confirmacionFinalizar === "1" || confirmacionFinalizar === "2") {
+    
+                if (confirmacionFinalizar === 1 || confirmacionFinalizar === 2 || confirmacionFinalizar === 3) {
                     break;
                 }
             }
